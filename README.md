cbea09ecdbbb95d1aa91fa5f8bad26ba40d101d2
Aditya Singh <79300029+aditya3011ai@users.noreply.github.com>
2022-08-31 08:31:57 +0530

    First commit
    Uploading my website

---
+++ README.md
@@ -0,0 +1,2 @@
+# GradientBackgrondGentrator.github.io
+This is a web page to get the best gradient background for you website.
---
+++ index.js
@@ -0,0 +1,23 @@
+var color1 = document.querySelector("#color1");
+var color2 = document.querySelector("#color2");
+var bg = document.querySelector("body");
+var h3 = document.querySelector("h3");
+var button = document.querySelector("button");
+var css;
+color1.addEventListener('input', ()=>{
+    button.textContent = "Copy css";
+    bg.style.backgroundImage="linear-gradient(to right,"+color1.value+","+color2.value+")";
+    h3.textContent = color1.value + "  " + color2.value;
+    css = "background-image: linear-gradient(to right," + h3.textContent + ");";
+});
+color2.addEventListener('input', () => {
+    button.textContent = "Copy css";
+    bg.style.backgroundImage="linear-gradient(to right,"+color1.value+","+color2.value+")" ;
+    h3.textContent = color1.value +" , "+  color2.value;
+    css = "background-image: linear-gradient(to right," + h3.textContent+");";
+});
+
+button.addEventListener('click',()=>{
+    navigator.clipboard.writeText( css);
+    button.textContent="Coppied!!";
+})
